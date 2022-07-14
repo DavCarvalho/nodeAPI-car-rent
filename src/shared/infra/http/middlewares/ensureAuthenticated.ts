119b8832cdff0e7f65703697b67a54e633972563
Davi Carvalho <davircsp@hotmail.com>
2022-07-14 00:53:21 -0300

    Middleware admi

--- src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -19,7 +19,8 @@ export async function ensureAuthenticated(request: Request, response: Response, 
   const [, token] = authHeader.split(" ");
 
   try {
-    const { sub: user_id } = verify(token, "2da89e5a6e7012ac4f6bc4a418c92ab4") as IPayload;
+    const { sub: user_id } = verify(token, "cfe275a5908b5650488e0b0342c2d6cc") as IPayload;
+    console.log(user_id)
 
     const usersRepository = new UsersRepository();
     usersRepository.findById(user_id);
