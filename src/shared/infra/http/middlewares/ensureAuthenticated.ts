c62cfb3f46491be52193b4434104c9770720e027
Davi Carvalho <davircsp@hotmail.com>
2022-07-15 00:09:56 -0300

    ListAvailableCars and test

--- src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -20,7 +20,7 @@ export async function ensureAuthenticated(request: Request, response: Response, 
 
   try {
     const { sub: user_id } = verify(token, "cfe275a5908b5650488e0b0342c2d6cc") as IPayload;
-    console.log(user_id)
+
 
     const usersRepository = new UsersRepository();
     usersRepository.findById(user_id);
